e1e781d3bf27039966d1a0677485ae95d28a9b1b
Wang Han <416810799@qq.com>
2016-11-02 18:44:44 +0800

    [Test] Rework Root Detection

--- app/src/main/java/com/ryansteckler/nlpunbounce/helpers/RootHelper.java
+++ app/src/main/java/com/ryansteckler/nlpunbounce/helpers/RootHelper.java
@@ -3,11 +3,8 @@ package com.ryansteckler.nlpunbounce.helpers;
 import android.os.Build;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class RootHelper {
@@ -19,40 +16,11 @@ public class RootHelper {
     }
 
     public static boolean isDeviceRooted() {
-        return checkRootMethod1() || checkRootMethod2() || checkRootMethod3() || checkRootMethod4();
-    }
-
-    public static boolean checkRootMethod1() {
-        String buildTags = android.os.Build.TAGS;
-        return buildTags != null && buildTags.contains("test-keys");
-    }
-
-    public static boolean checkRootMethod2() {
-        try {
-            File file = new File("/system/app/Superuser.apk");
-            return file.exists();
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public static boolean checkRootMethod4() {
-        try {
-            File file = new File("/system/xbin/su");
-            return file.exists();
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public static boolean checkRootMethod3() {
         return new ExecShell().executeCommand(ExecShell.SHELL_CMD.check_su_binary) != null;
     }
 
-
     public static void handleSELinux() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Process localProcess = null;
             try {
 
                 Process process = Runtime.getRuntime().exec(new String[]{"su"});
@@ -106,8 +74,6 @@ class ExecShell {
         } catch (Exception e) {
             return null;
         }
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-                localProcess.getOutputStream()));
         BufferedReader in = new BufferedReader(new InputStreamReader(
                 localProcess.getInputStream()));
         try {
